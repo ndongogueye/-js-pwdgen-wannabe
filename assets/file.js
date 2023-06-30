b904cf1603b92873c8e743334ebf641cd3a7b576
ndongogueye <bayeg2003@gmail.com>
2023-06-30 07:53:51 +0200

    javascript

---
+++ assets/file.js
@@ -0,0 +1,5 @@
+const Nome = prompt("Inserire il nome")
+const Cognome = prompt("Inserire il cognome")
+const Abitazione = prompt("Inserire il proprio colore preferito")
+
+document.getElementById("nome-preferito").innerHTML = '<h1>nomecognomecolorepreferito21</h1>'
